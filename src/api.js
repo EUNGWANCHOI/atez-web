7b210399051a47f239c9780582303df84e7f6ee0
EUNGWANCHOI <silvergwan@gmail.com>
2025-12-03 10:24:12 +0900

    update: be careful hack(보안 상승을 위한 랜덤 문자열 토큰)

--- src/api.js
+++ src/api.js
@@ -1,10 +1,9 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
 export async function sendMessageToAPI(message) {
-  const res = await fetch(API_URL, {
+  const res = await fetch(import.meta.env.VITE_API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      "X-CLIENT-TOKEN": import.meta.env.VITE_CLIENT_TOKEN,
     },
     body: JSON.stringify({ message }),
   });
