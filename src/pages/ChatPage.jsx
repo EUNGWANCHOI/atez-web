43939bd58cad4ee42ae749148953edff3e60fca4
EUNGWANCHOI <silvergwan@gmail.com>
2025-12-04 10:09:37 +0900

    feat: add profile picture(김민주 이쁘다 헤헤)

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,11 +1,15 @@
 import ChatWindow from "../components/ChatWindow";
 import "../index.css";
+import kmj from "../assets/kmj.png";
 
 export default function ChatPage() {
   return (
     <div className="chat-page">
       <div className="chat-container">
-        <div className="chat-header">유민아</div>
+        <div className="chat-header">
+          <img src={kmj} alt="김민주" />
+          유민아
+        </div>
         <ChatWindow />
       </div>
     </div>
